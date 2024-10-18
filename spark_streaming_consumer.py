ea93f8d2657ab41d4ae977943586c753fa55da64
w-nosliw <167265567+w-nosliw@users.noreply.github.com>
2024-10-18 10:46:37 -0500

    spark_streaming_consumer.py

--- spark_streaming_consumer.py
+++ spark_streaming_consumer.py
@@ -2,10 +2,14 @@ from pyspark.sql import SparkSession
 from pyspark.sql.functions import from_json, col, window
 from pyspark.sql.types import StructType, StructField, IntegerType, FloatType, TimestampType
 
-# Establecer nivel de logs en WARN
+# Configura el nivel de log a WARN para reducir los mensajes INFO
+spark=SparkSession.builder\
+  .appName("KafkaSparkStreaming")\
+  .getOrCreate()
+
 spark.sparkContext.setLogLevel("WARN")
 
-# Definir el esquema actualizado para los datos de entrada
+# Definir el esquema para los datos de entrada
 schema = StructType([
     StructField("sensor_id", IntegerType()),
     StructField("temperature", FloatType()),
@@ -17,37 +21,37 @@ schema = StructType([
 ])
 
 # Crear una sesión de Spark
-spark = SparkSession.builder \
-    .appName("SensorDataAnalysis") \
+spark = SparkSession.builder\
+    .appName("SensorDataAnalysis")\
     .getOrCreate()
 
 # Configurar el lector de streaming para leer desde Kafka
-df = spark \
-    .readStream \
-    .format("kafka") \
-    .option("kafka.bootstrap.servers", "localhost:9092") \
-    .option("subscribe", "sensor_data") \
+df = spark\
+    .readStream\
+    .format("kafka")\
+    .option("kafka.bootstrap.servers", "localhost:9092")\
+    .option("subscribe", "sensor_data")\
     .load()
 
 # Parsear los datos JSON
 parsed_df = df.select(from_json(col("value").cast("string"), schema).alias("data")).select("data.*")
 
 # Calcular estadísticas por ventana de tiempo (promedios por sensor y por minuto)
-windowed_stats = parsed_df \
-    .groupBy(window(col("timestamp"), "1 minute"), "sensor_id") \
+windowed_stats=parsed_df\
+    .groupBy(window(col("timestamp"), "1 minute"), "sensor_id")\
     .agg({
-        "temperature": "avg",
-        "humidity": "avg",
-        "pressure": "avg",
-        "battery_level": "avg",
-        "wind_speed": "avg"
+        "temperature":"avg",
+        "humidity":"avg",
+        "pressure":"avg",
+        "battery_level":"avg",
+        "wind_speed":"avg"
     })
 
 # Escribir los resultados en la consola
-query = windowed_stats \
-    .writeStream \
-    .outputMode("complete") \
-    .format("console") \
+query=windowed_stats\
+    .writeStream\
+    .outputMode("complete")\
+    .format("console")\
     .start()
 
 query.awaitTermination()
